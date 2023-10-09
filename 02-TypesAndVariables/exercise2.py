9a38eebb299e467d5e2daae6ce0f86e3228d3c99
Krystian Byrgiel <krysti248@gmail.com>
2023-10-09 17:52:33 +0200

    My first Python programs from textbook

---
+++ 02-TypesAndVariables/exercise2.py
@@ -0,0 +1,6 @@
+print("Enter your hours")
+hours = float(input())
+print("Enter your rate per hour")
+rate = float(input())
+pay = hours * rate
+print(pay)
---
+++ 02-TypesAndVariables/exercise3.py
@@ -0,0 +1,4 @@
+print("What is the temperature in Celsius?")
+Celsius = int(input())
+Fahrenheit = Celsius * 9/5 + 32
+print("Temperature in Fahrenheit is " , Fahrenheit)
